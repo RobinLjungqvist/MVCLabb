466396db901ef0a2c5dbbc38b5573d4d270102d1
Roppin.L <robin.ljungqvist@gmail.com>
2016-11-16 13:50:00 +0100

    Added a filterconfig and lets the project know we're using them in global asax. Also added an Authentication Controller

--- MVCLabb/MVCLabb/Global.asax.cs
+++ MVCLabb/MVCLabb/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MVCLabb.App_Start;
 
 namespace MVCLabb
 {
@@ -13,6 +14,7 @@ namespace MVCLabb
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters); // Säger till att filters skall användas när appen körs.
         }
     }
 }
